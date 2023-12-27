069e4895959ceb737847ed8590951253396f79f1
Rick van der Wal <rickvanderwalvis@gmail.com>
2023-12-27 16:54:31 +0100

    Day 12, part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,213 +1,102 @@
-use std::str::FromStr;
+use std::{str::FromStr, time::Instant};
 
-use rustc_hash::FxHashMap;
-
-type Cache<'a, 'b> = FxHashMap<(&'a [Spring], &'b [u32]), Result<u32, ()>>;
+use cached::proc_macro::cached;
 
 fn main() {
     let input = include_str!("../../inputs/input12");
 
-    println!("Part One: {}", solve_12a(input));
-    println!("Part Two: {}", solve_12b(input));
+    let start = Instant::now();
+    println!(
+        "Part One: {} ({}µs)",
+        solve_12a(input),
+        start.elapsed().as_micros()
+    );
+
+    let start = Instant::now();
+    println!(
+        "Part Two: {} ({}µs)",
+        solve_12b(input),
+        start.elapsed().as_micros()
+    );
 }
 
-fn solve_12a(input: &str) -> u32 {
+fn solve_12a(input: &str) -> u64 {
     let lines: Vec<_> = input
         .lines()
         .map(|line| Line::from_str(line).unwrap())
         .collect();
 
-    let mut cache = Cache::default();
-
     lines
-        .iter()
-        .map(|line| count_possibilities(&line.springs, &line.segments, &mut cache))
+        .into_iter()
+        .map(|line| Box::leak(Box::new(line)))
+        .map(|line| possibilities(&line.springs, &line.segments))
         .sum()
 }
 
-fn solve_12b(input: &str) -> u32 {
+fn solve_12b(input: &str) -> u64 {
     let lines: Vec<_> = input
         .lines()
         .map(|line| Line::from_str(line).unwrap())
         .map(|line| line.multiplied(5))
         .collect();
 
-    let mut cache = Cache::default();
-
     lines
-        .iter()
-        .map(|line| {
-            dbg!(count_possibilities(
-                &line.springs,
-                &line.segments,
-                &mut cache
-            ))
-        })
+        .into_iter()
+        .map(|line| Box::leak(Box::new(line)))
+        .map(|line| possibilities(&line.springs, &line.segments))
         .sum()
 }
 
-fn count_possibilities<'a, 'b>(
-    springs: &'a [Spring],
-    segments: &'b [u32],
-    cache: &mut Cache<'a, 'b>,
-) -> u32 {
-    // Get rid of trivial cases
-    let springs = trim_known_operational(springs);
-    if segments.iter().sum::<u32>() + segments.len() as u32 > springs.len() as u32 + 1 {
-        return 0;
-    }
-
-    // Find a functional spring in the middle
-    let functional = springs
-        .iter()
-        .enumerate()
-        .find_map(|(index, spring)| spring.is_operational().then_some(index));
-
-    match functional {
-        // No known functional springs, solve easier problem
-        None => count_possibilities_simple(springs, segments, cache).unwrap_or_default(),
-        // At least one functional spring, split the problem and combine the results
-        Some(index) => (0..=segments.len())
-            .map(|segments_split_index| {
-                let (left_segments, right_segments) = segments.split_at(segments_split_index);
-                count_possibilities(&springs[..index], left_segments, cache)
-                    * count_possibilities(&springs[index + 1..], right_segments, cache)
-            })
-            .sum(),
-    }
-}
-
-/// Counts the possibilities for a problem with no known functional springs.
-fn count_possibilities_simple<'a, 'b>(
-    springs: &'a [Spring],
-    segments: &'b [u32],
-    cache: &mut Cache<'a, 'b>,
-) -> Result<u32, ()> {
-    if let Some(&result) = cache.get(&(springs, segments)) {
-        return result;
-    }
-
-    match segments.first() {
-        // With 0 segments, there is exactly 1 possibility
-        None => Ok(1),
-        // Try placing the first segment everywhere, and solve the rest of the segments recursively
-        Some(&segment) => {
-            if segments.iter().sum::<u32>() + segments.len() as u32 > springs.len() as u32 + 1 {
-                // There is not enough space for the segments
-                cache.insert((springs, segments), Err(()));
-                return Err(());
-            }
-
-            if segment == springs.len() as u32 && segments.len() == 1 {
-                // Last one fits snugly
-                cache.insert((springs, segments), Ok(1));
-                return Ok(1);
-            }
-
-            let mut total = 0;
-
-            // Try placing the segment at the current leftmost possible position
-            if !springs[segment as usize].is_broken() {
-                total += count_possibilities_simple(
-                    &springs[segment as usize + 1..],
-                    &segments[1..],
-                    cache,
-                )
-                .unwrap_or_default();
-            }
-
-            // Don't try placing the segment at the current leftmost possible position
-            // Continue to the next unknown spring
-            total += match (1..springs.len()).find(|&index| springs[index - 1].is_unknown()) {
-                None => 0,
-                Some(index) => match strip_broken_prefix(&springs[index..], segments) {
-                    Ok((trimmed_springs, trimmed_segments)) => {
-                        count_possibilities_simple(trimmed_springs, trimmed_segments, cache)
-                            .unwrap_or_default()
-                    }
-                    Err(_) => 0,
-                },
-            };
-
-            cache.insert((springs, segments), Ok(total));
-            Ok(total)
-        }
-    }
-}
-
-#[must_use]
-fn trim_known_operational(springs: &[Spring]) -> &[Spring] {
-    let starts_getting_interesting = springs
-        .iter()
-        .copied()
-        .take_while(Spring::is_operational)
-        .count();
-    let ends_being_interesting = springs.len()
-        - springs
-            .iter()
-            .rev()
-            .copied()
-            .take_while(Spring::is_operational)
-            .count();
-    &springs[starts_getting_interesting..ends_being_interesting]
-}
-
-fn strip_broken_prefix<'a, 'b>(
-    mut springs: &'a [Spring],
-    mut segments: &'b [u32],
-) -> Result<(&'a [Spring], &'b [u32]), ()> {
-    assert!(!springs.contains(&Spring::Operational));
-
-    let new_start_index = after_broken_prefix_segment_index(springs, segments)?;
-    if new_start_index > 0 {
-        springs = &springs[new_start_index..];
-        segments = &segments[1..];
-    }
-
-    Ok((springs, segments))
-}
-
-fn after_broken_prefix_segment_index(springs: &[Spring], segments: &[u32]) -> Result<usize, ()> {
-    let prefixed_known_broken = springs
-        .iter()
-        .copied()
-        .take_while(Spring::is_broken)
-        .count();
-
-    if prefixed_known_broken > 0 {
-        let Some(remaining_segment) = segments
-            .first()
-            .and_then(|segment| segment.checked_sub(prefixed_known_broken as u32))
-        else {
-            // Too many are broken for the first segment
-            return Err(());
-        };
-
-        let segment_end = prefixed_known_broken + remaining_segment as usize;
-        if segment_end > springs.len() {
-            // Not enough space to complete the segment
-            return Err(());
-        }
-
-        if springs[prefixed_known_broken..segment_end]
-            .iter()
-            .all(Spring::might_be_broken)
-        {
-            if segment_end == springs.len() {
-                Ok(segment_end)
-            } else if springs[segment_end].might_be_operational() {
-                Ok(segment_end + 1)
+#[cached]
+fn possibilities(springs: &'static [Spring], segments: &'static [u32]) -> u64 {
+    match springs.first() {
+        None => {
+            if segments.is_empty() {
+                1
             } else {
-                // No space between this broken segment and the next
-                Err(())
+                0
             }
-        } else {
-            // Too few are broken for the first segment
-            Err(())
         }
-    } else {
-        Ok(0)
+        Some(Spring::Operational) => possibilities_first_operational(springs, segments),
+        Some(Spring::Broken) => possibilities_first_broken(springs, segments),
+        Some(Spring::Unknown) => {
+            let operational = possibilities_first_operational(springs, segments);
+            let broken = possibilities_first_broken(springs, segments);
+            operational + broken
+        }
     }
+}
+
+fn possibilities_first_broken(springs: &'static [Spring], segments: &'static [u32]) -> u64 {
+    match segments.first() {
+        None => 0,
+        Some(&segment) => {
+            if springs.len() >= segment as usize
+                && springs
+                    .iter()
+                    .take(segment as usize)
+                    .all(Spring::might_be_broken)
+            {
+                if springs.len() == segment as usize {
+                    if segments.len() == 1 {
+                        1
+                    } else {
+                        0
+                    }
+                } else if springs[segment as usize].might_be_operational() {
+                    possibilities(&springs[segment as usize + 1..], &segments[1..])
+                } else {
+                    0
+                }
+            } else {
+                0
+            }
+        }
+    }
+}
+
+fn possibilities_first_operational(springs: &'static [Spring], segments: &'static [u32]) -> u64 {
+    possibilities(&springs[1..], segments)
 }
 
 struct Line {
@@ -217,10 +106,13 @@ struct Line {
 
 impl Line {
     fn multiplied(mut self, multiplier: u32) -> Self {
-        for _ in 0..multiplier {
+        let original_springs = self.springs.clone();
+        let original_segments = self.segments.clone();
+
+        for _ in 0..multiplier - 1 {
             self.springs.push(Spring::Unknown);
-            self.springs.extend(self.springs.clone());
-            self.segments.extend(self.segments.clone());
+            self.springs.extend(original_springs.clone());
+            self.segments.extend(original_segments.clone());
         }
 
         self
@@ -247,18 +139,6 @@ enum Spring {
 }
 
 impl Spring {
-    fn is_operational(&self) -> bool {
-        *self == Self::Operational
-    }
-
-    fn is_broken(&self) -> bool {
-        *self == Self::Broken
-    }
-
-    fn is_unknown(&self) -> bool {
-        *self == Self::Unknown
-    }
-
     fn might_be_broken(&self) -> bool {
         *self != Self::Operational
     }
@@ -281,7 +161,7 @@ impl From<char> for Spring {
 
 #[cfg(test)]
 mod tests {
-    use crate::solve_12a;
+    use crate::{solve_12a, solve_12b};
 
     const INPUT: &str = "???.### 1,1,3
 .??..??...?##. 1,1,3
@@ -298,7 +178,6 @@ mod tests {
 
     #[test]
     fn test_12b() {
-        panic!("way too slow");
-        // assert_eq!(solve_12b(INPUT), 21)
+        assert_eq!(solve_12b(INPUT), 525152)
     }
 }
